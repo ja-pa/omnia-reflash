4cec41e3e3f4cc58cfcf53c38d25eb36833e2534
JAPA <ja-pa@users.noreply.github.com>
2017-06-15 12:16:31 +0200

    update: download key package with wget

--- update.sh
+++ update.sh
@@ -2,29 +2,33 @@
 
 update_repo() {
 
-	opkg update
-	opkg install cznic-repo-keys-test
+        package_name=$(curl http://repo.turris.cz/omnia-nightly/packages/turrispackages/|grep "cznic-repo-keys-test"|awk -F'href="' '{print $2}'|awk -F'"' '{print $1}')
+        wget http://repo.turris.cz/omnia-nightly/packages/turrispackages/$package_name
+        opkg install $package_name
+        #opkg install cznic-repo-keys-test
+        opkg update
 
-	uci set updater.override=override
-	uci set updater.override.branch=$1
-	uci commit
-	updater.sh
+        uci set updater.override=override
+        uci set updater.override.branch=$1
+        uci commit
+        updater.sh
 }
 
 case "$1" in
 "update")
-	if [ ! -z "$2" ]; then
-		echo "update repo"
-		update_repo $2
-	else 
-		echo "Error you have to set repo"
-	fi
+        if [ ! -z "$2" ]; then
+                echo "update repo"
+                update_repo $2
+        else
+                echo "Error you have to set repo"
+        fi
 ;;
 * | "--help")
-	echo "Help:"
-	echo "[command] [option]"
-	echo "Example:"
-	echo "update rc|stable|master|nightly"
+        echo "Help:"
+        echo "[command] [option]"
+        echo "Example:"
+        echo "update rc|stable|master|nightly"
 ;;
 
 esac
+
