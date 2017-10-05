8980eb236b514d2d450a75b9f72a9aefeab8bfc4
JAPA <ja-pa@users.noreply.github.com>
2017-10-05 11:49:08 +0200

    findpkg.py: add url

--- findpkg.py
+++ findpkg.py
@@ -47,9 +47,9 @@ class Packages:
 
     def get_pkg_list(self):
         feeds = ["base", "hardware", "lucics",
-               "management", "openwisp", "packages",
-               "php", "printing", "routing",
-               "telephony", "turrispackages"]
+                "management", "openwisp", "packages",
+                "php", "printing", "routing",
+                "telephony", "turrispackages"]
         print "Branch", self.__branch, ":"
         print "Downloading feeds :",
         for feed in feeds:
@@ -60,6 +60,23 @@ class Packages:
             except:
                 print feed+"(not found),",
         print
+
+    def _get_gitlab_url(self, pkg_source, branch="test"):
+        ret = ""
+        if pkg_source.startswith("feeds"):
+            if pkg_source.startswith("feeds/turrispackages"):
+                ret = pkg_source.replace("feeds/turrispackages/",
+                                        "https://gitlab.labs.nic.cz/turris/turris-os-packages/tree/%s/" % branch)
+            elif pkg_source.startswith("feeds/lucics"):
+                ret = pkg_source.replace("feeds/lucics/",
+                                        "https://gitlab.labs.nic.cz/turris/luci-cs/tree/master/")
+            return ret
+        elif pkg_source.startswith("package"):
+            ret = pkg_source.replace("package/",
+                                    "https://gitlab.labs.nic.cz/turris/openwrt/tree/%s/" % branch)
+            return ret
+        else:
+            return ""
 
     def _download_list(self, feed):
         response = urllib2.urlopen(self.__repo_url % (self.__branch, feed))
@@ -74,6 +91,9 @@ class Packages:
         for item in tmp:
             if item != [""]:
                 ret.append(item)
+        tmp_dict = dict(ret)
+        if "Source" in tmp_dict:
+            ret.append(["GitlabURL", self._get_gitlab_url(tmp_dict["Source"])])
         return dict(ret)
 
     def _parse_packages(self, packages):
@@ -103,9 +123,10 @@ def print_pkg(packages, header=["Package", "Version", "Filename"]):
 
 
 def main_cli(argv):
+    global abc
     header = ["Package", "Version", "Filename"]
     parser = argparse.ArgumentParser(description='Omnia')
-    parser.add_argument('-fp', '--find-package',nargs='+', help='find package')
+    parser.add_argument('-fp', '--find-package', nargs='+', help='find package')
     parser.add_argument('-fd', '--find-depends', help='find depend packages')
     parser.add_argument('-b', '--branch', action="store", default="omnia-nightly",
                         help='set omnia branch')
@@ -117,6 +138,9 @@ def main_cli(argv):
                         const="Source", help='Print print source', default=None)
     parser.add_argument('-ppd', '--print-package-depends', action="store_const",
                         const="Depends", help='Print depends source', default=None)
+    parser.add_argument('-pu', '--print-url', action="store_const",
+                        const="GitlabURL", help='Print gitlab url', default=None)
+
     args = parser.parse_args(argv)
     if args.print_description:
         header.append(args.print_description)
@@ -126,12 +150,14 @@ def main_cli(argv):
         header.append(args.print_source)
     if args.print_package_depends:
         header.append(args.print_package_depends)
+    if args.print_url:
+        header.append(args.print_url)
     if args.find_package:
         abc = Packages(args.branch)
         abc.get_pkg_list()
-        ccc=[]
-        for pkg_name in  args.find_package:
-            ccc+=abc.search_by_name(pkg_name, False)
+        ccc = []
+        for pkg_name in args.find_package:
+            ccc += abc.search_by_name(pkg_name, False)
         table = AsciiTable(print_pkg(ccc, header))
         print "Find ", args.find_package, "in branch ", args.branch
         print
