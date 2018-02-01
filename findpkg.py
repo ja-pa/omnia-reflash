84a476c6fe8a7db0c33d4a418b74823eae1d54db
JAPA <ja-pa@users.noreply.github.com>
2018-02-01 14:09:58 +0100

    findpkg.py: add -pl option for searching lede repo

--- findpkg.py
+++ findpkg.py
@@ -14,8 +14,14 @@ import sys
 class Packages:
     def __init__(self, branch="omnia-nightly"):
         self.__branch = branch
-        self.__repo_url = 'http://repo.turris.cz/%s/packages/%s/Packages'
+        self.__repo_url_turris = 'http://repo.turris.cz/%s/packages/%s/Packages'
+        self.__repo_url_lede = 'https://downloads.lede-project.org/snapshots/packages/x86_64/%s/Packages'
         self._pkg_list = []
+        self.__feeds_turris = ["base", "hardware", "lucics", "management",
+                               "openwisp", "packages", "php", "printing",
+                               "routing", "telephony", "turrispackages"]
+        self.__feeds_lede = ["base", "luci", "packages",
+                             "routing", "telephony"]
 
     def search_by_name(self, name, case_sensitive=True):
         ret = []
@@ -45,16 +51,17 @@ class Packages:
     def search_by_sha256(self, sha256):
         pass
 
-    def get_pkg_list(self):
-        feeds = ["base", "hardware", "lucics",
-                "management", "openwisp", "packages",
-                "php", "printing", "routing",
-                "telephony", "turrispackages"]
-        print "Branch", self.__branch, ":"
+    def get_pkg_list(self, project):
+        if project == "turris":
+            feeds = self.__feeds_turris
+            print "Branch", self.__branch, ":"
+        else:
+            feeds = self.__feeds_lede
+            print "Lede repo (latest snapshot:"
         print "Downloading feeds :",
         for feed in feeds:
             try:
-                feed_list = self._download_list(feed)
+                feed_list = self._download_list(feed, project)
                 print feed + ", ",
                 self._pkg_list = self._pkg_list + self._parse_packages(feed_list)
             except:
@@ -66,20 +73,25 @@ class Packages:
         if pkg_source.startswith("feeds"):
             if pkg_source.startswith("feeds/turrispackages"):
                 ret = pkg_source.replace("feeds/turrispackages/",
-                                        "https://gitlab.labs.nic.cz/turris/turris-os-packages/tree/%s/" % branch)
+                                         "https://gitlab.labs.nic.cz/turris/turris-os-packages/tree/%s/" % branch)
             elif pkg_source.startswith("feeds/lucics"):
                 ret = pkg_source.replace("feeds/lucics/",
-                                        "https://gitlab.labs.nic.cz/turris/luci-cs/tree/master/")
+                                         "https://gitlab.labs.nic.cz/turris/luci-cs/tree/master/")
             return ret
         elif pkg_source.startswith("package"):
             ret = pkg_source.replace("package/",
-                                    "https://gitlab.labs.nic.cz/turris/openwrt/tree/%s/" % branch)
+                                     "https://gitlab.labs.nic.cz/turris/openwrt/tree/%s/" % branch)
             return ret
         else:
             return ""
 
-    def _download_list(self, feed):
-        response = urllib2.urlopen(self.__repo_url % (self.__branch, feed))
+    def _download_list(self, feed, project="turris"):
+        """ Download package feed list for project (lede,turris ) """
+        if project == "turris":
+            url_full = self.__repo_url_turris % (self.__branch, feed)
+        else:
+            url_full = self.__repo_url_lede % feed
+        response = urllib2.urlopen(url_full)
         return response.read()
 
     def _parse_signle_package(self, package):
@@ -126,20 +138,27 @@ def main_cli(argv):
     global abc
     header = ["Package", "Version", "Filename"]
     parser = argparse.ArgumentParser(description='Omnia')
-    parser.add_argument('-fp', '--find-package', nargs='+', help='find package')
+    parser.add_argument('-fp', '--find-package', nargs='+',
+                        help='find package')
     parser.add_argument('-fd', '--find-depends', help='find depend packages')
-    parser.add_argument('-b', '--branch', action="store", default="omnia-nightly",
-                        help='set omnia branch')
+    parser.add_argument('-b', '--branch', action="store",
+                        default="omnia-nightly", help='set omnia branch')
+    parser.add_argument('-pl', '--project-lede', action="store_true",
+                        help='Search in lede project')
     parser.add_argument('-pd', '--print-description', action="store_const",
-                        const="Description", help='Print description', default=None)
+                        const="Description", help='Print description',
+                        default=None)
     parser.add_argument('-ps', '--print-section', action="store_const",
                         const="Section", help='Print section', default=None)
     parser.add_argument('-pss', '--print-source', action="store_const",
-                        const="Source", help='Print print source', default=None)
+                        const="Source", help='Print print source',
+                        default=None)
     parser.add_argument('-ppd', '--print-package-depends', action="store_const",
-                        const="Depends", help='Print depends source', default=None)
+                        const="Depends", help='Print depends source',
+                        default=None)
     parser.add_argument('-pu', '--print-url', action="store_const",
-                        const="GitlabURL", help='Print gitlab url', default=None)
+                        const="GitlabURL", help='Print gitlab url',
+                        default=None)
 
     args = parser.parse_args(argv)
     if args.print_description:
@@ -154,7 +173,10 @@ def main_cli(argv):
         header.append(args.print_url)
     if args.find_package:
         abc = Packages(args.branch)
-        abc.get_pkg_list()
+        if args.project_lede:
+            abc.get_pkg_list("lede")
+        else:
+            abc.get_pkg_list("turris")
         ccc = []
         for pkg_name in args.find_package:
             ccc += abc.search_by_name(pkg_name, False)
