a943f152cca2e6d2248f667b1686a67a5b9988be
Jan Pavlinec <jan.pavlinec@nic.cz>
2021-01-26 11:10:35 +0100

    update_all.sh: update with new modes

--- update_all.sh
+++ update_all.sh
@@ -5,20 +5,41 @@ if [ -d packages ]; then
 fi
 
 update_openwrt_master() {
- 	git clone https://github.com/ja-pa/openwrt
- 	cd openwrt
- 	git remote add upstream https://github.com/openwrt/openwrt.git
- 	git fetch upstream
- 	git pull upstream master
+	git clone https://github.com/ja-pa/openwrt
+	cd openwrt
+	git remote add upstream https://github.com/openwrt/openwrt.git
+	git fetch upstream
+	git pull upstream master
+	git push
+}
+
+update_openwrt_branch() {
+	branch="$1"
+
+	git clone https://github.com/ja-pa/openwrt
+	cd openwrt/
+	git remote add upstream https://github.com/openwrt/openwrt.git
+	git fetch upstream
+	git checkout --track origin/$branch
+	git pull upstream $branch
 	git push
 }
 
 update_master() {
- 	git clone https://github.com/ja-pa/packages
- 	cd packages/
- 	git remote add upstream https://github.com/openwrt/packages.git
- 	git fetch upstream
- 	git pull upstream master
+	git clone https://github.com/ja-pa/packages
+	cd packages/
+	git remote add upstream https://github.com/openwrt/packages.git
+	git fetch upstream
+	git pull upstream master
+	git push
+}
+
+update_master_luci() {
+	git clone https://github.com/ja-pa/luci
+	cd luci
+	git remote add upstream https://github.com/openwrt/luci.git
+	git fetch upstream
+	git pull upstream master
 	git push
 }
 
@@ -41,6 +62,8 @@ print_help() {
 	echo openwrt-18.06
 	echo openwrt-19.07
 	echo openwrt-master openwrt branch
+	echo openwrt-main-19.07 openwrt branch
+	echo luci-master
 }
 
 cmd="$1"
@@ -49,6 +72,10 @@ case $cmd in
 		echo "Update $cmd"
 		update_master
 	;;
+	openwrt-main-18.06|openwrt-main-19.07)
+		echo "Update $cmd"
+		update_openwrt_branch openwrt-19.07
+	;;
 	openwrt-master)
 		echo "Update $cmd"
 		update_openwrt_master
@@ -56,6 +83,10 @@ case $cmd in
 	openwrt-18.06|openwrt-19.07)
 		echo "Update $cmd"
 		update_branch $cmd
+	;;
+	luci-master)
+		echo "Update $cmd"
+		update_master_luci
 	;;
 	*)
 	       print_help
